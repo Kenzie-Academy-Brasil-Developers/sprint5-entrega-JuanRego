9c5f4dec637af2089876995f9c6301aa0b20821e
Juan Rego da Silva Rocha <juanrego.s@outlook.com>
2022-05-15 15:47:29 -0300

    fix

--- src/routers/users/users.routes.ts
+++ src/routers/users/users.routes.ts
@@ -2,13 +2,15 @@ import { Router } from "express";
 import userCreateController from "../../controllers/user/userCreate.controller";
 import userListController from "../../controllers/user/userList.controller";
 import userListOneController from "../../controllers/user/userListOne.controller";
+import userDeleteController from "../../Controllers/user/userDelete.Controller";
+import userPatchController from "../../Controllers/user/userPatch.Controller";
 
 const routes = Router();
 
 routes.post("/users", userCreateController);
 routes.get("/users", userListController);
 routes.get("/users/:id", userListOneController);
-routes.patch("/users/:id");
-routes.delete("/users/:id");
+routes.patch("/users/:id", userPatchController);
+routes.delete("/users/:id", userDeleteController);
 
 export default routes;
